7cbeb118a1ea31c6d35da1c777b424f1be0eef77
always-op <103885998+always-op@users.noreply.github.com>
2022-04-18 17:25:44 +0530

    Delete Backend directory

--- Backend/Routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { createProduct, getAllProducts } = require('../Controllers/productController')
-const router = express.Router()
-
-router.route('/products/new').post(createProduct)
-router.route('/products').get(getAllProducts)
-
-
-module.exports = router
